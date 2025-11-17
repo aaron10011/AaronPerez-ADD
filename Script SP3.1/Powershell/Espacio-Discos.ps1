b77a345d25ee02a0f9a746c96511119b45e9a4b0
Aaron Perez <aaronpr117@gmail.com>
2025-11-17 16:44:58 +0000

    Scripts SP3.1 PS

---
+++ Script SP3.1/Powershell/Espacio-Discos.ps1
@@ -0,0 +1,12 @@
+﻿# Aviso-EspacioDisco.ps1
+# Muestra un aviso si el disco C: tiene poco espacio libre
+
+$disco = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'"
+$espacioLibreGB = $disco.FreeSpace / 1GB
+$espacioTotalGB = $disco.Size / 1GB
+$porcentajeLibre = ($espacioLibreGB / $espacioTotalGB) * 100
+
+if ($porcentajeLibre -lt 15) {
+    Add-Type -AssemblyName PresentationFramework
+    [System.Windows.MessageBox]::Show("El disco C: tiene menos de un 15% de espacio libre. Por favor, elimina archivos innecesarios o mueve datos a otro disco.", "Espacio en disco bajo", 'OK', 'Warning')
+}
--- Script SP3.1/Powershell/aaronpsp31-3eve.ps1
+++ Script SP3.1/Powershell/aaronpsp31-3eve.ps1
@@ -1,32 +1,33 @@
 ﻿[int]$opcion = -1
+
 while ($opcion -ne 0){
 
-    $valores = Get-EventLog -LogName System | Select-Object -ExpandProperty EntryType -Unique
+    $valores = Get-EventLog -List | Select-Object -ExpandProperty Log
     $i = 1
 
-    Write-Host "`n===== MENU DE EVENTOS =====`n"
+    Write-Host "`n===== MENU DE TIPOS DE REGISTROS =====`n"
     foreach ($valor in $valores){
         Write-Host "$i. $valor"
         $i++
     }
     Write-Host "0. Salir`n"
 
-    [int]$opcion = Read-Host "Seleccione una opcion (1, 2, 3...)"
+    [int]$opcion = Read-Host "Seleccione una opción (1, 2, 3...)"
 
     if ($opcion -eq 0){
-
         Write-Host "`nSaliendo..."
+    }
+    elseif ($opcion -ge 1 -and $opcion -le $valores.Count){
 
-    } elseif ([int]$opcion -ge 1 -and [int]$opcion -le $valores.Count){
-       
-       $seleccion = $valores[$opcion - 1]
+        $seleccion = $valores[$opcion - 1]
 
-       Get-EventLog -LogName System -EntryType $seleccion -Newest 12
-        
-    } else{
+        Write-Host "`nMostrando los 12 últimos eventos del registro: $seleccion"
+        Write-Host "==================================================`n"
 
-        Write-Host "`nOpción Inválida, vuelva a intentarlo" -ForegroundColor Red
+        Get-EventLog -LogName $seleccion -Newest 12 -ErrorAction SilentlyContinue
 
     }
-
+    else {
+        Write-Host "`nOpción inválida, vuelva a intentarlo" -ForegroundColor Red
+    }
 }
